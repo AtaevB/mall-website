6993a606a5864b2242b2905a596ed6bd3f858e5e
badrik <badrik_ataev@mail.ru>
2024-10-17 19:08:01 +0300

    0.2 (home done)

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,5 +1,8 @@
 import './footer.css';
 import logo from '../../images/logo.png';
+import telegram from '../../images/icons/telegram.png';
+import email from '../../images/icons/email.png';
+import vkontakte from '../../images/icons/vkontakte.png';
 
 const Footer = () => {
     return ( <>
@@ -9,17 +12,35 @@ const Footer = () => {
 
                 <div className='footer-logo'><a href=""><img src={logo} alt='Logo'/></a></div>
 
-                <div className='footer-wrapper'>
+                <div className='icon-wrapper'>
 
                     <ul>
 
-                        <li>1</li>
-                        <li>2</li>
-                        <li>3</li>
+                        <li>
+                        <a href=''><img src={telegram} alt='icon' className='footer-icon' /></a>
+                        </li>
+
+                        <li>
+                        <a href=''><img src={email} alt='icon' className='footer-icon' /></a>
+                        </li>
+
+                        <li>
+                        <a href=''><img src={vkontakte} alt='icon' className='footer-icon' /></a>
+                        </li>
 
                     </ul>
 
                 </div>
+        
+                <div className='contact-wrapper'>
+
+                    <ul>
+                        <li><p>+7 (977) 388 84-50</p></li>
+                        <li><p>Москва, улица Московская, д.1</p></li>
+                    </ul>
+
+                </div>
+
             </div>
         </footer>
 
